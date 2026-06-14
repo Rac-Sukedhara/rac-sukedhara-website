7ed8961ddcc28e6a21d3d4f9ddd8e1c218aec1d2
Sushovan Shakya <sushakya235@proton.me>
2026-06-14 13:43:14 +0545

    changed website metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Rotaract Club of Sukedhara | RID 3292",
+  title: "Rotaract Club of Sukedhara | District 3292",
   description: "",
 };
 
